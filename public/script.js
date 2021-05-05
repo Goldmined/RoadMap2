55bd8f6c0ef964e3c8bda3c8301e6f41b6daa0c6
Goldmined <goldmined666@gmail.com>
2021-05-05 16:39:47 +0300

    Task1_4

---
+++ public/script.js
@@ -0,0 +1,17 @@
+// const { json } = require("express")
+
+fetch('http://localhost:3001/users')
+.then((res)=> res.json()) // обработать промис
+.then((data) => {
+    console.log(data);
+    const usersEl = document.querySelector('.users')
+    for (let item of data.items){
+        const userEl = document.createElement('div');
+        userEl.classList.add('user');
+        userEl.innerHTML = `
+        <h3>${item.name}</h3>
+        <p>${item.email}</p>
+        `
+        usersEl.appendChild(userEl)
+    }
+})
